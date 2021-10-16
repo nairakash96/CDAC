50345506763c0183ffdd147a1f5ad646de24210c
Akash Ashokan Nair <78254350+nairakash96@users.noreply.github.com>
2021-10-16 19:47:44 +0530

    Update Product.java

--- Concept Of Programming/Assignment 9/src/Product.java
+++ Concept Of Programming/Assignment 9/src/Product.java
@@ -14,7 +14,7 @@ public class Product {
 	static int getProduct(int num1,int num2) {
 		if(num1==0||num2==0)
 			return 0;
-	return num1 + getProduct(num1,num2-1);	
+	return num1 + getProduct(num1,num2-1);
 	}
 
 }
